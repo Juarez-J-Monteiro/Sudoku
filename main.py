49212b3dd3123e6155051ccdc02476d110c48c1f
Juarez-J-Monteiro <juarezjrdossantos2014@gmail.com>
2026-08-15 01:47:17 -0300

    feat: add basic validation for grid and formatting inputs

--- main.py
+++ main.py
@@ -1,2 +1,4 @@
 from src.jogo import Jogo
 
+jogo = Jogo()
+jogo.iniciarPartida()
--- src/jogo.py
+++ src/jogo.py
@@ -1,13 +1,47 @@
-class Jogo():
+from time import sleep
 
-    def gerarTabuleiro():
+class Jogo:
+    def __init__(self):
         pass
 
-    def exibirTabuleiro():
+    def gerarTabuleiro(self):
         pass
 
-    def verificaJogada():
+    def exibirTabuleiro(self):
+        pass
+
+    def verificaJogada(self):
         pass
     
-    def fazerJogada():
+    def fazerJogada(self):
         pass
+
+    def iniciarPartida(self):
+        while True:
+            inputUsuario = str(input("Insira sua jogada (linha, coluna, número):\n"))
+
+            # Validacao do input
+            try:
+                valores = inputUsuario.split(",")
+                inputLinha = int(valores[0])
+                inputColuna = int(valores[1])
+                inputNumero = int(valores[2])
+
+                # Validacao do intervalo inserido, avança o laço caso esteja fora do intervalo
+                if not (1 <= inputLinha <= 9):
+                    print("\n[Erro]: A LINHA escolhida ({}) nao existe. Escolha de 1 a 9.\n".format(inputLinha))
+                    continue
+                elif not (1 <= inputColuna <= 9):
+                    print("\n[Erro]: A COLUNA escolhida ({}) nao existe. Escolha de 1 a 9.\n".format(inputColuna))
+                    continue
+                elif not (1 <= inputNumero <= 9):
+                    print("\n[Erro]: O NUMERO escolhido ({}) nao existe. Escolha de 1 a 9.\n".format(inputNumero))
+                    continue
+                
+            # Trata erro de formato invalido no input
+            except Exception:
+                print("\n[Erro] Entrada inválida: '{}'\n".format(inputUsuario))
+                inputUsuario = ""
+                continue
+
+            # print("L: {}, C: {}, N: {}".format(inputLinha, inputColuna, inputNumero))
