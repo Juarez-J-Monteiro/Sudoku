c2f454764d888f3d4936115c6a80840a6686a95f
Juarez-J-Monteiro <juarezjrdossantos2014@gmail.com>
2026-08-15 00:13:33 -0300

    init: project initialization

---
+++ main.py
@@ -0,0 +1,2 @@
+from src.jogo import Jogo
+
---
+++ src/jogo.py
@@ -0,0 +1,13 @@
+class Jogo():
+
+    def gerarTabuleiro():
+        pass
+
+    def exibirTabuleiro():
+        pass
+
+    def verificaJogada():
+        pass
+    
+    def fazerJogada():
+        pass
