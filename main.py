3bec61bcbb649bc45ea885245731a9810ff6b886
Juarez-J-Monteiro <juarezjrdossantos2014@gmail.com>
2026-08-20 22:38:44 -0300

    feat(main): add play again mechanic after game ends

--- main.py
+++ main.py
@@ -1,4 +1,32 @@
 from src.jogo import Jogo
 
-jogo = Jogo()
-jogo.iniciarPartida()
+def iniciarJogo():
+    """Cria uma nova instância do jogo e inicia a partida."""
+    jogo = Jogo()
+    jogo.iniciarPartida()
+
+def perguntarJogarNovamente():
+    """Pergunta ao usuário se deseja jogar novamente. Trata respostas inválidas"""
+
+    while True:
+        resposta = input("\nDeseja jogar novamente? (s/n)").strip().lower()
+        if resposta == "s":
+            return True
+        elif resposta == 'n':
+            return False
+        else:
+            print("Resposta inválida! Digite `s` para SIM ou `n` para NÃO!")
+
+def main():
+    """Controla a "vida" do programa, inciando partidas quando optado pelo usuário. Inicia automaticamente
+    a primeira partida do programa"""
+
+    jogarNovamente = True
+
+    while jogarNovamente:
+        iniciarJogo()
+        jogarNovamente = perguntarJogarNovamente()
+
+    print("Obrigado por jogar!")
+
+main()
